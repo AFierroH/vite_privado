66ee5d86ae404c2fceb905d451972156ee477c5a
ArminFH <82161874+AFierroH@users.noreply.github.com>
2025-11-26 13:35:01 -0300

    Ventas2 agregado https quitado puerto

--- web/src/api.js
+++ web/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = import.meta.env.VITE_API_URL || 'https://miposra.site:3000/api'
+export const API_URL = import.meta.env.VITE_API_URL || 'https://miposra.site/api'
 
 const api = axios.create({
   baseURL: API_URL,
